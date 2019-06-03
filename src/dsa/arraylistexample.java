5d5f5817ff3a10a65ee14081a81d9a376ba10835
unknown <malinduruwantha@gmail.com>
2019-06-03 17:22:26 +0530

    arraylist creation

---
+++ src/dsa/arraylistexample.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa;
+import java.util.*;
+/**
+ *
+ * @author Malindu
+ */
+public class arraylistexample {
+public void arraylistcreate(){
+ArrayList<String> a=new ArrayList<String>();
+
+a.add("malindu");
+a.add("amith");
+a.add("ayesh");
+a.add("isuru");
+a.add("lanka");
+
+System.out.println(a);
+
+a.add(4,"bhathiya");
+System.out.println(a);
+
+}  
+}
